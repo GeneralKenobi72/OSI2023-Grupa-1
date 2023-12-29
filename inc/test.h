9082808d8e70ee9a609ed621051ed48b780b98d1
Luka Zeljko <lukazeljko2002@gmail.com>
2023-12-29 12:59:10 +0100

    Dodaj CMakeLists.txt

---
+++ inc/test.h
@@ -0,0 +1,16 @@
+#pragma once
+#include<iostream>
+
+class test {
+	private: 
+		std::string value;
+
+	public:
+		test(std::string value) {
+			this->value = value;
+		}
+
+		std::string getValue() {
+			return value;
+		}
+};
---
+++ src/test.cpp
@@ -0,0 +1,7 @@
+#include "../inc/test.h"
+
+int main() {
+	test t1 = test("test");
+
+	std::cout << t1.getValue();
+}
