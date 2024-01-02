f921f61249e6dd697338fb0cc2c0175fb1551d95
Bosko Brankovic <boskobrankovic8@gmail.com>
2024-01-02 14:46:47 +0100

    Revert "Sitne promjene u testnom fajlu"
    This reverts commit 6438dee982f2b9514f861e8f44503b042fd61392.
    vracanje prethodnih promjena, upotreba git revert
    	#	modified:   src/test.cpp #

--- src/test.cpp
+++ src/test.cpp
@@ -2,8 +2,7 @@
 #include <iostream>
 
 int main() {
-	test t1 = test("kevin panter, probace preko jabuselea...\n\n\n\n\n\nOAAAAAAA KEVIN PANTER\n\nvreme nije isteklo");
+	test t1 = test("test");
 
 	std::cout << t1.getValue();
-	return 0;
 }
